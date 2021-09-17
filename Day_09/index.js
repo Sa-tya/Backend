538098f3e956ffaad403ecebb3f7ab56672fd015
Satya Prakash <80380695+Sa-tya@users.noreply.github.com>
2021-09-18 00:47:53 +0530

    updated

--- Day_09/index.js
+++ Day_09/index.js
@@ -6,21 +6,31 @@ const app = express();
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','jade');
 
-app.use('/',(req,res)=>{
+app.all('/abc',(req,res,next)=>{
+    req.query.name += ' singh';
+    next();
+})
+app.use('/',(req,res,next)=>{
     // console.log('file sended...',__dirname)
     // res.sendFile(path.join(__dirname,'Demo.txt'),'Hello.txt')
     // res.download(path.join(__dirname,'Demo.txt'),'Hello.txt')
     // res.render('index',{title:'Static file'})
-    res.status(201)
-    .cookie('hello','hello1',{
-        expires : new Date(Date.now() + 60000)
-    })
-    .cookie('refer','refered')
-    .redirect(301,'https://google.com')
+    // res.status(201)
+    // .cookie('hello','hello1',{
+    //     expires : new Date(Date.now() + 60000)
+    // })
+    // .cookie('refer','refered')
+    // .redirect(301,'/admin')
+    req.query.name += ' prakash'
+    next();
 })
 
-// app.get('/',(req,res)=>{
-//     res.send('Hello...')
-// })
+app.get('/',(req,res)=>{
+    // res.sendFile(path.join(__dirname,'Demo.txt'),'Hello.txt')
+    res.send('Hello...'+req.query.name)
+})
 
+app.get('/abc',(req,res)=>{
+    res.send('Hello...'+req.query.name)
+})
 app.listen(3333);
